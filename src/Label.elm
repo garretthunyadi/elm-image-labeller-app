27a4d564433ed36f6a29f0e204db80242b7a1374
Garrett Hunyadi <garrett.hunyadi@endurance.com>
2019-11-02 16:27:51 -0400

    more

--- src/Label.elm
+++ src/Label.elm
@@ -4,6 +4,11 @@ module Label exposing (Domain, main)
 
 [ ] Where do I get my input?
 
+[ ] Color coding
+
+[ ] Bug: Label names should always be generated dynamically, in case the schema changes.
+Or should it?
+
 [ ] Configure Labels
 [ ] Show/Hide Label Configuration section
 [ ] Add a list for labels containing the text labels
@@ -27,11 +32,11 @@ Approach 3: Manipulating labels, referencing only S3, generating a script to be 
 -- import Bool.Extra
 
 import Browser
-import Html exposing (Html, button, div, img, input, pre, span, text, textarea)
-import Html.Attributes exposing (cols, height, rows, src, type_, value, width)
-import Html.Events exposing (on, onClick, onInput)
+import Html exposing (Html, button, div, img, pre, text, textarea)
+import Html.Attributes exposing (class, cols, height, rows, src, value, width)
+import Html.Events exposing (onClick, onInput)
 import List exposing (map)
-import List.Extra as LE exposing (zip)
+import List.Extra as LE
 
 
 
@@ -67,19 +72,6 @@ makeLabels strings =
     List.map2 (\index name -> { index = index, name = name }) (List.range 0 (List.length strings)) strings
 
 
-
--- labels : List Image -> List Label
--- labels images =
---     let
---         maybeLabel : Image -> Maybe Label
---         maybeLabel image =
---             Maybe.map
---                 (\l -> l)
---                 image.label
---     in
---     List.filterMap maybeLabel images
-
-
 imagesWithLabels : List Image -> List ( Image, Label )
 imagesWithLabels images =
     let
@@ -95,35 +87,19 @@ imagesWithLabels images =
     List.filterMap maybeLabel images
 
 
-jsonLineFor : Image -> Label -> String
-jsonLineFor image label =
-    "{\"source-ref\":\"" ++ imageUrl image.domain ++ "\", \"class\":\"" ++ String.fromInt label.index ++ "\", \"class-metadata\": {\"class-name\": \"" ++ label.name ++ "\"}}\n"
-
-
 
 -- {"source-ref":"s3://image/filename1.jpg", "class":"0"}
 -- {"source-ref":"s3://image/filename2.jpg", "class":"1", "class-metadata": {"class-name": "cat", "type" : "groundtruth/image-classification"}}
 
 
+jsonLineFor : Image -> Label -> String
+jsonLineFor image label =
+    "{\"source-ref\":\"" ++ imageUrl image.domain ++ "\", \"class\":\"" ++ String.fromInt label.index ++ "\", \"class-metadata\": {\"class-name\": \"" ++ label.name ++ "\"}}\n"
+
+
 jsonLabels : List Image -> List String
 jsonLabels images =
     map (\( i, l ) -> jsonLineFor i l) (imagesWithLabels images)
-
-
-
--- map (\i l -> "") (imagesWithLabels images)
--- [ "aaa", "bbb" ]
--- map jsonLineFor (imagesWithLabels images)
--- map (\( i, l ) -> "") (imagesWithLabels images)
--- let
---     maybeJsonLabel : Image -> Maybe String
---     maybeJsonLabel image =
---         Maybe.map
---             (\l -> "{\"source-ref\":\"" ++ imageUrl image.domain ++ "\", \"class\":\"" ++ String.fromInt l.index ++ "\"}\n")
---             image.label
--- in
--- List.filterMap maybeJsonLabel images
--- map (\l -> "{\"source-ref\":\"" ++ imageUrl image.domain ++ "\", \"class\":\"" ++ String.fromInt l.index ++ "\"}\n") (labels images)
 
 
 type alias Image =
@@ -135,13 +111,6 @@ type alias Image =
 imageUrl : Domain -> String
 imageUrl domain =
     "https://eiginsites.s3.amazonaws.com/sites/" ++ domain ++ "/screenshots_600x600.jpeg"
-
-
-
--- s3Url : Domain -> String
--- s3Url domain =
---     -- TODO incorrect
---     "s3://sites/" ++ domain ++ "/screenshots_600x600.jpeg"
 
 
 type alias Model =
@@ -289,40 +258,7 @@ nextLabel list maybeItem =
 
 
 
--- cycleWithBlank ls ml
-
-
-cycleWithBlank : List a -> a -> Maybe a
-cycleWithBlank list a =
-    let
-        getAt2 : List a -> Int -> Maybe a
-        getAt2 l i =
-            LE.getAt i l
-
-        -- getNext : a -> List a -> Maybe a
-        getNext item l =
-            list
-                |> LE.elemIndex item
-                |> Maybe.map ((+) 1)
-                |> Maybe.andThen (getAt2 l)
-    in
-    case getNext a list of
-        Just next ->
-            Just next
-
-        Nothing ->
-            LE.getAt 0 list
-
-
-
--- case ml of
---     Just l ->
---         cycleWithBlank ls l
---     Nothing ->
---         List.head ls
--- viewInput : String -> String -> (String -> msg) -> Html msg
--- viewInput p v toMsg =
---     input [ type_ "text", placeholder p, value v, onInput toMsg ] []
+-- View
 
 
 view : Model -> Html Msg
@@ -391,28 +327,52 @@ viewLabelConfig model =
         ]
 
 
-
--- viewLabelEditor : Label -> Html Msg
--- viewLabelEditor label =
---     div []
---         [ -- viewInput label.name label.name (onInput (UpdateLabelsWithLabel label))
---         --   viewInput label.name UpdateLabelWithName
---             textarea (map name la
---         ]
--- viewInput : String -> (String -> msg) -> Html msg
--- viewInput v toMsg =
---     input [ type_ "text", value v, onInput toMsg ] []
-
-
 viewImage : Int -> Int -> Image -> Html Msg
 viewImage w h image =
     img
         [ src (imageUrl image.domain)
         , width w
         , height h
+        , class (cssClass image.label)
         , onClick (LabelChange image)
         ]
         []
+
+
+cssClass : Maybe Label -> String
+cssClass maybeLabel =
+    case maybeLabel of
+        Just l ->
+            "label-" ++ String.fromInt l.index
+
+        _ ->
+            "unlabelled"
+
+
+
+-- adjust for now having css capabilities
+
+
+cssColor : Maybe Label -> String
+cssColor maybeLabel =
+    let
+        colors =
+            [ "red", "green", "blue", "yellow", "purple", "orange" ]
+
+        colorAtIndex i =
+            case LE.getAt i colors of
+                Just c ->
+                    c
+
+                Nothing ->
+                    "error_color_455023"
+    in
+    case maybeLabel of
+        Just l ->
+            "label-" ++ colorAtIndex l.index
+
+        _ ->
+            ""
 
 
 viewImages : Int -> Int -> List Image -> List (Html Msg)
@@ -467,25 +427,6 @@ makeImage d =
 
 
 -- helper
--- getWanted : List a -> a -> Maybe a
--- getWanted list currentElement =
---     let
---         findNextInList l =
---             case l of
---                 [] ->
---                     Nothing
---                 x :: [] ->
---                     if x == currentElement then
---                         List.head list
---                     else
---                         Nothing
---                 x :: y :: rest ->
---                     if x == currentElement then
---                         Just y
---                     else
---                         findNextInList (y :: rest)
---     in
---     findNextInList list
 
 
 dumpJsonLabelsAsPre : List Image -> Html Msg
